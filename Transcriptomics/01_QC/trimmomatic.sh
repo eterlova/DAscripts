e0fa5a13a3fe648670c19286ea9d3cf8ae61687f
Elizaveta Terlova <elizaveta.terlova@uconn.edu>
2021-05-10 13:06:35 -0400

    Create trimmomatic.sh
    ** Problematic
    
    ignores samples 1-3, not sure if something is wrong about the loop or not

---
+++ Transcriptomics/01_QC/trimmomatic.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=Trimmomatic
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH --mem=100G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=elizaveta.terlova@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+module load Trimmomatic/0.39
+
+for i in {1..36}
+do java -jar $Trimmomatic PE -threads 4 \
+        /home/CAM/eterlova/2021DA_transcriptomes/data/LTervola_RNASeq_April2021/ET*_S$i*_R1_001.fastq.gz \
+        /home/CAM/eterlova/2021DA_transcriptomes/data/LTervola_RNASeq_April2021/ET*_S$i*_R2_001.fastq.gz \
+        trim_S{$i}_R1.fastq.gz singles_trim_S{$i}_R1.fastq.gz \
+        trim_S{$i}_R2.fastq.gz singles_trim_S{$i}_R2.fastq.gz \
+        ILLUMINACLIP:/isg/shared/apps/Trimmomatic/0.36/adapters/TruSeq3-PE-2.fa:2:30:10 \
+        SLIDINGWINDOW:4:25 MINLEN:45
+done
