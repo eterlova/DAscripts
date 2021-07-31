53bbd12d2b611f2bdb5dbd761914cdd6e040156e
Elizaveta Terlova <elizaveta.terlova@uconn.edu>
2021-07-31 16:35:01 -0400

    Update Preprocessing_XCMS_IPO.R

--- Metabolomics/Preprocessing_XCMS_IPO.R
+++ Metabolomics/Preprocessing_XCMS_IPO.R
@@ -1,7 +1,7 @@
-if (!requireNamespace("BiocManager", quietly=TRUE))
-  install.packages("BiocManager")
-BiocManager::install("IPO")
-install.packages("xcms", "RColorBrewer", "pander", "magrittr", "pheatmap", "SummarizedExperiment", "data.table")
+#if (!requireNamespace("BiocManager", quietly=TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install("IPO")
+#install.packages("xcms", "RColorBrewer", "pander", "magrittr", "pheatmap", "SummarizedExperiment", "data.table")
 
 library(xcms)
 library(RColorBrewer)
@@ -24,8 +24,8 @@ samples <- c("sample_013", "sample_026", "sample_056", "sample_063", "sample_110
              "sample_412", "sample_417", "sample_432", "sample_444", "sample_452", "sample_457", "sample_464",
              "sample_469", "sample_485", "sample_518", "sample_532", "sample_577", "sample_582", "sample_583",
              "sample_588", "sample_590", "sample_602", "sample_609", "sample_615")
-  
-rawfiles <- vector()  
+
+rawfiles <- vector()
 
 for(i in samples){
   add <- dir(path="~/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/Data", pattern=i, full.names = TRUE,
@@ -33,24 +33,35 @@ for(i in samples){
   rawfiles[[length(rawfiles)+1]] <- add
 }
 rawfiles
-fwrite(list(rawfiles), file = "sample_paths.txt")
+#fwrite(list(rawfiles), file = "sample_paths.txt")
 
-pooled <- dir(path="~/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/Data", pattern="pulled", full.names = TRUE,
+pooled_files <- dir(path="~/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/Data", pattern="pulled", full.names = TRUE,
               recursive = TRUE)
-fwrite(list(pooled), file = "pooled_paths.txt")
+#fwrite(list(pooled), file = "pooled_paths.txt")
 
-blanks <- dir(path="~/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/Data", pattern="MeOH")
-fwrite(list(blanks), file = "blanks_paths.txt")
+blanks_files <- dir(path="~/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/Data", pattern="MeOH",full.names = TRUE,
+              recursive = TRUE)
+#fwrite(list(blanks), file = "blanks_paths.txt")
 
 # 1.2 read in phenotypes
 
-phenotypes <- read.table("C:/Users/LisaTerlova/Documents/DAmetabolomics/ZA17_JT2_CCAP_alpha/ZA17_JT2_CCAP_alpha_phenotypes.csv",
+phenotypes <- read.table("/home/CAM/eterlova/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/ZA17_JT2_CCAP_alpha_phenotypes.csv",
                        header=TRUE,
+                       sep=",")
+pooled_phenotypes <- read.table("/home/CAM/eterlova/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/pooled_phenotypes.csv",
+                    header=TRUE,
+                       sep=",")
+blanks_phenotypes <- read.table("/home/CAM/eterlova/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/blanks_phenotypes.csv",
+                    header=TRUE,
                        sep=",")
 
 # 2. read the files
 
 raw_data <- readMSData(files = rawfiles, pdata = new("NAnnotatedDataFrame", phenotypes),
+                       mode = "onDisk")
+pooled <- readMSData(files = pooled_files, pdata = new("NAnnotatedDataFrame", pooled_phenotypes),
+                       mode = "onDisk")
+blanks <- readMSData(files = blanks_files, pdata = new("NAnnotatedDataFrame", blanks_phenotypes),
                        mode = "onDisk")
 #raw_data_test <- filterRt(raw_data, c(250, 350))
 
@@ -72,30 +83,29 @@ plot(bpis, col = group_colors[raw_data_test$Strain])
 # QC samples should suffice for parameter optimization (polled samples?)
 
 # peak detection with matchedFilter parameter optimization
-#peakpickingParameters_matchedFilter <- getDefaultXcmsSetStartingParams('matchedFilter')
+peakpickingParameters_matchedFilter <- getDefaultXcmsSetStartingParams('matchedFilter')
 #setting levels for step to 0.2 and 0.3 (hence 0.25 is the center point)
-#peakpickingParameters$step <- c(0.2, 0.3)
-#peakpickingParameters$fwhm <- c(40, 50)
+peakpickingParameters_matchedFilter$step <- c(0.2, 0.3)
+peakpickingParameters_matchedFilter$fwhm <- c(40, 50)
 #setting only one value for steps therefore this parameter is not optimized
-#peakpickingParameters$steps <- 2
+peakpickingParameters_matchedFilter$steps <- 2
 
-#time.xcmsSet <- system.time({ # measuring time
-#  resultPeakpicking_matchedFilter <- 
-#    optimizeXcmsSet(files = rawfiles, #insert list of polled files instead
-#                    params = peakpickingParameters, 
-#                    nSlaves = 1, 
-#                    subdir = NULL,
-#                    plot = TRUE)
-#})
-
-#best parameter settings: also saved in resultPeakpicking
-#fwhm: 9.3
-#snthresh: 1
-#step: 0.2884
+time.xcmsSet <- system.time({ # measuring time
+  resultPeakpicking_matchedFilter <-
+    optimizeXcmsSet(files = pooled_files,
+                    params = peakpickingParameters_matchedFilter,
+                    nSlaves = 1, 
+                    subdir = NULL,
+                    plot = FALSE)
+})
+#Best parameters:
+#fwhm: 15
+#snthresh: 6.22112
+#step: 0.28848
 #steps: 2
-#sigma: 3.94937998980805
+#sigma: 6.36996772549686
 #max: 5
-#mzdiff: 0.2232
+#mzdiff: 0.22304
 #index: FALSE
 
 
@@ -108,9 +118,9 @@ peakpickingParameters_centWave$value_of_prefilter <- c(200,300)
 
 time.xcmsSet <- system.time({ # measuring time
   resultPeakpicking_centWave <- 
-    optimizeXcmsSet(files = pooled,
+    optimizeXcmsSet(files = pooled_files,
                     params = peakpickingParameters_centWave, 
-                    nSlaves = 4, 
+                    nSlaves = 1, 
                     subdir = NULL,
                     plot = FALSE)
 })
@@ -131,13 +141,26 @@ time.xcmsSet <- system.time({ # measuring time
 
 
 # 4.2 centroiding data (required by CentWave algorithm)
-# verify smoothing options!!!
 # Biochanin m/z 285.0767 rt 5.30695
 # Lidocaine m/z 235.1811 rt 3.15795
 
-raw_data_test_cent <- raw_data_test %>% smooth(method = "SavitzkyGolay", halfWindowSize = 4L) %>% pickPeaks()
-bpis_cent <- chromatogram(raw_data_test_cent, aggregationFun="max")# base peak chromatograms (BPC)
-plot(bpis_cent, col = group_colors[raw_data_test_cent$Strain])
+# 4.2.1 smoothing data in the m/z dimention using Savitzky-Golay filter with a half-window size of 4
+raw_data_sg <- raw_data %>%
+    smooth(method = "SavitzkyGolay", halfWindowSize = 4L)
+pooled_sg <- pooled %>%
+    smooth(method = "SavitzkyGolay", halfWindowSize = 4L)
+blanks_sg <- blanks %>%
+    smooth(method = "SavitzkyGolay", halfWindowSize = 4L)
+
+# 4.2.2. centroiding data using pickPeaks() with descendPeak m/z refinement
+# descendPeak walks, on both sides from the centroid, down until the signal
+#is below signalPercentage% of the centroid’s intensity (by default 33%), or until the signal increases again. 
+#All m/z intensity pairs within this range are used for the weighted average calculation of the centroid’s m/z value.
+raw_data_cent <- raw_data_sg %>% pickPeaks(refineMz = "descendPeak")
+pooled_cent <- pooled_sg %>% pickPeaks(refineMz = "descendPeak")
+blanks_cent <- blanks_sg %>% pickPeaks(refineMz = "descendPeak")
+
+
 
 # 4.3 chromatographic peak detection
 xchr <- findChromPeaks(raw_data_test_cent, param = CentWaveParam(ppm = 32,
