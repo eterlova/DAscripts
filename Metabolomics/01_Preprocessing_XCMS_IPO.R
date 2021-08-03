b30cc2c6c3edab25276a36d632b5100d2f785412
Elizaveta Terlova <elizaveta.terlova@uconn.edu>
2021-08-03 08:30:22 -0400

    Update and rename Preprocessing_XCMS_IPO.R to 01_Preprocessing_XCMS_IPO.R

--- Metabolomics/01_Preprocessing_XCMS_IPO.R
+++ Metabolomics/01_Preprocessing_XCMS_IPO.R
@@ -11,11 +11,10 @@ library(pheatmap)
 library(SummarizedExperiment)
 library(IPO)
 library(data.table)
-#NB use normal slash (/) for file paths despite what Windows does
 
 # 1.1 save path of each sample in a vector
 #"sample_502" #does not exist
-#samples <- c("sample_013", "sample_026")
+##samples <- c("sample_013", "sample_026")
 samples <- c("sample_013", "sample_026", "sample_056", "sample_063", "sample_110", "sample_111", "sample_127",
              "sample_128", "sample_133", "sample_137", "sample_157", "sample_166", "sample_169", "sample_174",
              "sample_176", "sample_190", "sample_193", "sample_216", "sample_217", "sample_224", "sample_245",
@@ -24,24 +23,26 @@ samples <- c("sample_013", "sample_026", "sample_056", "sample_063", "sample_110
              "sample_412", "sample_417", "sample_432", "sample_444", "sample_452", "sample_457", "sample_464",
              "sample_469", "sample_485", "sample_518", "sample_532", "sample_577", "sample_582", "sample_583",
              "sample_588", "sample_590", "sample_602", "sample_609", "sample_615")
+  
 
-rawfiles <- vector()
+
+rawfiles <- vector()  
 
 for(i in samples){
   add <- dir(path="~/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/Data", pattern=i, full.names = TRUE,
              recursive = TRUE)
   rawfiles[[length(rawfiles)+1]] <- add
 }
-rawfiles
-#fwrite(list(rawfiles), file = "sample_paths.txt")
+##rawfiles
+##fwrite(list(rawfiles), file = "sample_paths.txt")
 
 pooled_files <- dir(path="~/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/Data", pattern="pulled", full.names = TRUE,
               recursive = TRUE)
-#fwrite(list(pooled), file = "pooled_paths.txt")
+##fwrite(list(pooled), file = "pooled_paths.txt")
 
 blanks_files <- dir(path="~/2021DA_metabolomes/ZA17_JT2_CCAP_alpha_data/Data", pattern="MeOH",full.names = TRUE,
               recursive = TRUE)
-#fwrite(list(blanks), file = "blanks_paths.txt")
+##fwrite(list(blanks), file = "blanks_paths.txt")
 
 # 1.2 read in phenotypes
 
@@ -65,40 +66,27 @@ blanks <- readMSData(files = blanks_files, pdata = new("NAnnotatedDataFrame", bl
                        mode = "onDisk")
 #raw_data_test <- filterRt(raw_data, c(250, 350))
 
-# 3. initial data inspection
-group_colors <- paste0(brewer.pal(3, "Set1"), "60")
-names(group_colors) <- c("ZA1-7", "CCAP 276/35", "JT2-VF29")
-
-# Get the base peak chromatograms
-bpis <- chromatogram(raw_data_test, aggregationFun="max")# base peak chromatograms (BPC)
-plot(bpis, col = group_colors[raw_data_test$Strain])
-
-# Get the total ion current by file
-#tc <- split(tic(raw_data_test), f = fromFile(raw_data_test))
-#boxplot(tc, col = group_colors[raw_data_test$Strain],
-#        ylab = "intensity", main = "Total ion current")
-
-# 4. chromatographic peak detection
-# 4.1 parameter optimization with IPO package
+# 4 parameter optimization with IPO package
 # QC samples should suffice for parameter optimization (polled samples?)
 
-# peak detection with matchedFilter parameter optimization
-peakpickingParameters_matchedFilter <- getDefaultXcmsSetStartingParams('matchedFilter')
+# 4.1 parameter optimization with matchedFilter
+##peakpickingParameters_matchedFilter <- getDefaultXcmsSetStartingParams('matchedFilter')
 #setting levels for step to 0.2 and 0.3 (hence 0.25 is the center point)
-peakpickingParameters_matchedFilter$step <- c(0.2, 0.3)
-peakpickingParameters_matchedFilter$fwhm <- c(40, 50)
+##peakpickingParameters_matchedFilter$step <- c(0.2, 0.3)
+##peakpickingParameters_matchedFilter$fwhm <- c(40, 50)
 #setting only one value for steps therefore this parameter is not optimized
-peakpickingParameters_matchedFilter$steps <- 2
+##peakpickingParameters_matchedFilter$steps <- 2
 
-time.xcmsSet <- system.time({ # measuring time
-  resultPeakpicking_matchedFilter <-
-    optimizeXcmsSet(files = pooled_files,
-                    params = peakpickingParameters_matchedFilter,
-                    nSlaves = 1, 
-                    subdir = NULL,
-                    plot = FALSE)
-})
-#Best parameters:
+##time.xcmsSet <- system.time({ # measuring time
+##  resultPeakpicking_matchedFilter <- 
+##    optimizeXcmsSet(files = pooled_files, 
+##                    params = peakpickingParameters_matchedFilter, 
+##                    nSlaves = 1, 
+##                    subdir = NULL,
+##                    plot = FALSE)
+##})
+
+#Best parameters (pooled,matchedFilter):
 #fwhm: 15
 #snthresh: 6.22112
 #step: 0.28848
@@ -108,43 +96,12 @@ time.xcmsSet <- system.time({ # measuring time
 #mzdiff: 0.22304
 #index: FALSE
 
-
-# peak detection with centWave
-peakpickingParameters_centWave <- getDefaultXcmsSetStartingParams('centWave')
-peakpickingParameters_centWave$min_peakwidth <- c(5,14)
-peakpickingParameters_centWave$max_peakwidth <- c(14,35)
-peakpickingParameters_centWave$prefilter <- c(2,3)
-peakpickingParameters_centWave$value_of_prefilter <- c(200,300)
-
-time.xcmsSet <- system.time({ # measuring time
-  resultPeakpicking_centWave <- 
-    optimizeXcmsSet(files = pooled_files,
-                    params = peakpickingParameters_centWave, 
-                    nSlaves = 1, 
-                    subdir = NULL,
-                    plot = FALSE)
-})
-
-#best parameter settings:
-#min_peakwidth: 11.75
-#max_peakwidth: 32.9
-#ppm: 32
-#mzdiff: -0.001
-#prefilter: 2
-#value_of_prefilter: 200
-#snthresh: 10
-#noise: 0
-#mzCenterFun: wMean
-#integrate: 1
-#fitgauss: FALSE
-#verbose.columns: FALSE
-
-
 # 4.2 centroiding data (required by CentWave algorithm)
 # Biochanin m/z 285.0767 rt 5.30695
 # Lidocaine m/z 235.1811 rt 3.15795
 
 # 4.2.1 smoothing data in the m/z dimention using Savitzky-Golay filter with a half-window size of 4
+
 raw_data_sg <- raw_data %>%
     smooth(method = "SavitzkyGolay", halfWindowSize = 4L)
 pooled_sg <- pooled %>%
@@ -159,23 +116,79 @@ blanks_sg <- blanks %>%
 raw_data_cent <- raw_data_sg %>% pickPeaks(refineMz = "descendPeak")
 pooled_cent <- pooled_sg %>% pickPeaks(refineMz = "descendPeak")
 blanks_cent <- blanks_sg %>% pickPeaks(refineMz = "descendPeak")
+#
 
+# 4.3 parameter optimization with centWave
+peakpickingParameters_centWave <- getDefaultXcmsSetStartingParams('centWave')
+peakpickingParameters_centWave$min_peakwidth <- c(5,14)
+peakpickingParameters_centWave$max_peakwidth <- c(14,35)
+peakpickingParameters_centWave$prefilter <- c(2,3)
+peakpickingParameters_centWave$value_of_prefilter <- c(200,300)
 
+# 4.3.1 based on pooled files
+time.xcmsSet <- system.time({ # measuring time
+  resultPeakpicking_centWave_pooled <- 
+    optimizeXcmsSet(files = pooled_files,
+                    params = peakpickingParameters_centWave, 
+                    nSlaves = 1, 
+                    subdir = NULL,
+                    plot = FALSE)
+}) 
 
-# 4.3 chromatographic peak detection
-xchr <- findChromPeaks(raw_data_test_cent, param = CentWaveParam(ppm = 32,
-                                                                 peakwidth = c(9.5, 35),
-                                                                 mzdiff = -0.001,
-                                                                 prefilter = c(2, 200),
-                                                                 snthresh = 10,
-                                                                 noise = 0,
-                                                                 mzCenterFun = "wMean",
-                                                                 integrate = 1,
-                                                                 fitgauss = FALSE,
-                                                                 extendLengthMSW = FALSE))
-head(chromPeaks(xchr))
-chromPeakData(xchr)
+#best parameter settings (pooled, centWave):
+#min_peakwidth: 11.75
+#max_peakwidth: 32.9
+#ppm: 32
+#mzdiff: -0.001
+#prefilter: 2
+#value_of_prefilter: 200
+#snthresh: 10
+#noise: 0
+#mzCenterFun: wMean
+#integrate: 1
+#fitgauss: FALSE
+#verbose.columns: FALSE
 
-# 4.4 refine detected peaks # de  before or after filtering?
+# 4.3.2 based on blank files
+time.xcmsSet <- system.time({ # measuring time
+  resultPeakpicking_centWave_blanks <-
+    optimizeXcmsSet(files = blanks_files,
+                    params = peakpickingParameters_centWave,
+                    nSlaves = 1,
+                    subdir = NULL,
+                    plot = FALSE)
+})
 
+# best parameter settings (blanks, centWave):
+#min_peakwidth: 14
+#max_peakwidth: 35
+#ppm: 32
+#mzdiff: -0.001
+#prefilter: 2
+#value_of_prefilter: 200
+#snthresh: 10
+#noise: 0
+#mzCenterFun: wMean
+#integrate: 1
+#fitgauss: FALSE
+#verbose.columns: FALSE
 
+print("Optimize retention time correction and grouping parameters based on pooled samples")
+# 4.4 Optimize retention time correction and grouping parameters
+retcorGroupParameters <- getDefaultRetGroupStartingParams()
+retcorGroupParameters$profStep <- 1
+retcorGroupParameters$gapExtend <- 2.7
+
+time.RetGroup <- system.time({ # measuring time
+resultRetcorGroup <-
+  optimizeRetGroup(xset = resultPeakpicking_centWave_pooled$best_settings$xset,
+                   params = retcorGroupParameters,
+                   nSlaves = 10,
+                   subdir = NULL,
+                   plot = FALSE)
+})
+
+writeRScript(resultPeakpicking_centWave_pooled$best_settings$parameters,
+             resultRetcorGroup$best_settings)
+time.xcmsSet
+time.RetGroup
