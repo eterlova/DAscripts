4394aabc21dcedd3f3c2bb900a675e84c0a9204c
Elizaveta Terlova <elizaveta.terlova@uconn.edu>
2021-08-23 06:32:08 -0400

    Update PeakDetection1.sh

--- Metabolomics/PeakDetection1.sh
+++ Metabolomics/PeakDetection1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=02_PeakDetection
+#SBATCH --job-name=PeakDetection-1
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH -c 1
@@ -14,19 +14,19 @@
 
 dir="~/2021DA_metabolomes/02_PeakDetection_wconda"
 file_list="~/2021DA_metabolomes/01_IPO_parameters_pooled/samples_paths.txt"
-out_dir="~/2021DA_metabolomes/02_PeakDetection_wconda/$SLURM_JOB_ID"
+out_dir="$SLURM_JOB_ID"
 #if [ ! -d "$out_dir" ]; then
 mkdir "$out_dir"
 #fi
 
 cd "$dir"
 
-source ~/anaconda3/etc/profile.d/conda.sh
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate Rmetab
 
 Rscript --no-save --no-restore --verbose 02.1_PeakDetection.R \
 "$file_list" "$SLURM_ARRAY_TASK_ID" "out_dir" \
-> "~/2021DA_metabolomes/02_PeakDetection_wconda/outfile_"$SLURM_JOB_ID".Rout" 2>&1 > "$SLURM_JOB_ID".log
+> "outfile_"$SLURM_JOB_ID".Rout" 2>&1 > "$SLURM_JOB_ID".log
 
-cat *.log > all.log
+cat *.log > all.log 
 
