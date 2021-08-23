4744c938b656abdceef559dc88dfa7769e236e33
Elizaveta Terlova <elizaveta.terlova@uconn.edu>
2021-08-23 06:24:40 -0400

    Update PeakDetection1.sh

--- Metabolomics/PeakDetection1.sh
+++ Metabolomics/PeakDetection1.sh
@@ -6,23 +6,27 @@
 #SBATCH --mem=4G
 #SBATCH --partition=general
 #SBATCH --qos=general
-#SBATCH --array=[0-50] ## put here how many files you have
+#SBATCH --array=[0-52] ## put here how many files you have
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elizaveta.terlova@uconn.edu
 #SBATCH -o %x_%A_%a.out
 #SBATCH -e %x_%A_%a.err
 
 dir="~/2021DA_metabolomes/02_PeakDetection_wconda"
-file_list="~/2021DA_metabolomes/01_IPO_parameters_pooled/blanks_paths.txt"
-out_dir="~/2021DA_metabolomes/02_PeakDetection_wconda"
+file_list="~/2021DA_metabolomes/01_IPO_parameters_pooled/samples_paths.txt"
+out_dir="~/2021DA_metabolomes/02_PeakDetection_wconda/$SLURM_JOB_ID"
 #if [ ! -d "$out_dir" ]; then
-#  mkdir "$out_dir"
+mkdir "$out_dir"
 #fi
 
 cd "$dir"
 
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate Rmetab
 
 Rscript --no-save --no-restore --verbose 02.1_PeakDetection.R \
 "$file_list" "$SLURM_ARRAY_TASK_ID" "out_dir" \
-> "~/2021DA_metabolomes/02_PeakDetection_wconda/outfile_$SLURM_JOB_ID.Rout" 2>&1 > $out_dir/$SLURM_JOB_ID.log
+> "~/2021DA_metabolomes/02_PeakDetection_wconda/outfile_"$SLURM_JOB_ID".Rout" 2>&1 > "$SLURM_JOB_ID".log
+
+cat *.log > all.log
+
