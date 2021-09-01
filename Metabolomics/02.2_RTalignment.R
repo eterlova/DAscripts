96d72d20ea5e49ba9fd27e1627103244ea83dc5a
Elizaveta Terlova <elizaveta.terlova@uconn.edu>
2021-09-01 09:10:06 -0400

    Update 02.2_RTalignment.R

--- Metabolomics/02.2_RTalignment.R
+++ Metabolomics/02.2_RTalignment.R
@@ -1,4 +1,9 @@
-labrary()
+library(data.table)
+library(pander)
+library(magrittr)
+library(pheatmap)
+library(SummarizedExperiment)
+labrary(xcms)
 
 ## Input dir
 input_dir <- paste0(args[1])
@@ -19,24 +24,45 @@ for(i in 2:length(input_l)) {
   xset <- c(xset, set)
   }
   
-## Grouping
-gset <- group(xset,  , method="density")
-save(list=c("gset"), file=paste0(output_dir,"/02.2.1_Grouping-1.RData"))
+## Grouping for parameters see script 01 or outfiles of that job
+#gset <- group(xset,  , method="density")
+#save(list=c("gset"), file=paste0(output_dir,"/02.2.1_Grouping-1.RData"))
 
-## RT correction
-rset <- retcor.peakgroup(gset, plottype="none", ...)
-save(list=c("rset"), file=paste0(output_dir,"/02.2.2_RTcorrection.RData"))
+## RT correction based on groups for parameters see script 01 or outfiles of that job
+#rset <- retcor.peakgroup(gset, plottype="none", ...)
+#save(list=c("rset"), file=paste0(output_dir,"/02.2.2_RTcorrection.RData"))
+
+## RT correction obiwarp
+xdata <- adjustRtime(xset, param = ObiwarpParam(binSize = 0.4)) #somebody on the internet varied bin size between 0.2 and 2
+write.csv(rtime(xdata), "./02.2.1_Samples_AdjustedTime.csv", row.names = TRUE)
+save(list=c("xdata"), file=paste0(output_dir,"02.2.1_RTcorrection.Rdata"))
+
+## Evaluate the RT alignment
+## Get the base peak chromatograms.
+bpis_adj <- chromatogram(xdata, aggregationFun = "max", include = "none")
+par(mfrow = c(2, 1), mar = c(4.5, 4.2, 1, 0.5))
+plot1<-plot(bpis_adj, col = group_colors[bpis_adj$sample_group])
+## Plot also the difference of adjusted to raw retention time.
+plot2 <- plotAdjustedRtime(xdata)
+
+pdf("./02.2.1_Samples_RTcorrection_BPC.pdf") 
+plot
+dev.off()
+
+pdf("./02.2.1_Samples_RTcorrection_AdjustedTimeDifference.pdf") 
+plot
+dev.off()
 
 ## Grouping No2
-grset <- group(rset, method="density", ...)
-save(list=c("grset"), file=paste0(output_dir,"/02.2.3_Grouping-2.RData"))
+#grset <- group(rset, method="density", ...)
+#save(list=c("grset"), file=paste0(output_dir,"/02.2.3_Grouping-2.RData"))
 
 ## Filling peaks
-fset <- fillPeaks(grset,
-                 method="chrom",
-                 BPPARAM = MulticoreParm(worker=bw))
-save(list=c("fset"), file=paste0(output_dir,"/02.2.4_FillingPeaks.RData"))
+#fset <- fillPeaks(grset,
+#                 method="chrom",
+#                 BPPARAM = MulticoreParm(worker=bw))
+#save(list=c("fset"), file=paste0(output_dir,"/02.2.4_FillingPeaks.RData"))
 
-Table=peakTable(fset)
-write.csv(Table, file=paste0(output_dir, "/02.2.5_Peak_Table_Name.csv"), col.names=FALSE)
+#Table=peakTable(fset)
+#write.csv(Table, file=paste0(output_dir, "/02.2.5_Peak_Table_Name.csv"), col.names=FALSE)
 
